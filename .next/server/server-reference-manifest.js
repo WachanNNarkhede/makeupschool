c1f872aad6fb2b1abed83f3de20cb2ec52543613
Scamwac <wachannarkhede4@gmail.com>
2025-05-15 04:25:01 +0530

    commit

--- .next/server/server-reference-manifest.js
+++
@@ -1,1 +0,0 @@
-self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"r+JQiIN/XOUwgoRljil3LCsqUMJ2DL2vke4eyDbVXH0=\"\n}"
--- .next/static/chunks/app_layout_tsx_c0237562._.js
+++
@@ -1,9 +0,0 @@
-(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
-    script: typeof document === "object" ? document.currentScript : undefined,
-    chunks: [
-  "static/chunks/_4b5ec26c._.css",
-  "static/chunks/node_modules_a1af30e5._.js",
-  "static/chunks/components_4fa74a7b._.js"
-],
-    source: "dynamic"
-});
